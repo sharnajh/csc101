cb071c9f6efb329e2bae583f61da422b2270017e
Sharna Hossain <sharnajh@gmail.com>
2020-07-23 21:13:52 -0400

    changes

--- chapter9.py
+++ chapter9.py
@@ -342,20 +342,21 @@ print(f'Hours:\t{hours}\nMinutes:\t{minutes}\nSeconds:\t{seconds}')
 
 # Program 18
 # future_value.py
-# Get the desired future value.
-future_value = float(input('Enter the desired future value:\t'))
+# Compressing all the steps into a function
+def calculate_amount():
+    # Get the desired future value.
+    future_value = float(input('Enter the desired future value:\t'))
+    # Get the annual interest rate.
+    rate = float(input('Enter the annual interest rate:\t'))
+    # Get the number of years that the money will appreciate
+    years = int(input('Enter the number of years the money will grow:\t'))
+    # Calculate the amount that needed to deposit and return
+    present_value = future_value / (1.0 + rate)**years
+    # Display the amount needed to deposit
+    return print(f'You will need to deposit this amount: {present_value}')
 
-# Get the annual interest rate.
-rate = float(input('Enter the annual interest rate:\t'))
-
-# Get the number of years that the money will appreciate
-years = int(input('Enter the number of years the money will grow:\t'))
-
-# Calculate the amount that needed to deposit
-present_value = future_value / (1.0 + rate)**years
-
-# Display the amount needed to deposit
-print(f'You will need to deposit this amount: {present_value}')
+# Executing function
+calculate_amount()
 
 
 # Program 19
