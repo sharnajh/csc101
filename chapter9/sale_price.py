63eeeb40fcf3cf9ac82121a17425c2eb1427e08e
Sharna Hossain <sharnajh@gmail.com>
2020-07-25 17:18:51 -0400

    added pictures and func

--- chapter9/sale_price.py
+++ chapter9/sale_price.py
@@ -5,9 +5,11 @@
 # In my modification of Program 15, I have compiled the
 # discount algorithm into a function and am using the
 # convertToUSD helper function to format the return
+# I am also generating a random discount with the randomNum()
+# helper function
 
 # Importing convertToUSD() from helper_functons.py
-from helper_functions import convertToUSD
+from helper_functions import convertToUSD, randomNum
 
 # This program gets an item's original price and
 # calculates its sales price, with a 20% discount.
@@ -15,14 +17,22 @@ from helper_functions import convertToUSD
 original_price = float(input("Enter the item's original price:\t"))
 
 # I created a function to compute the discount.
-def calculate_discount(price,discount):
+
+
+def calculate_discount(price, discount):
     # Calculate the amount of the discount.
     discount = price * discount
     # Calculate the sales price.
     return price - discount
 
-sale_price = calculate_discount(original_price,.2)
+# Generating random discount with random() function
+discount = randomNum("float", .1, .9)
 
-# Display the sale price.
-# Using the convertToUSD() helper function
+# Executing calculate_discount function and storing it
+sale_price = calculate_discount(original_price, discount)
+
+# Display the formatted discount value as a percentage
+print(f'The discount is {"{:.2f}".format(discount * 100)}%')
+
+# Using the convertToUSD() helper function to format sale pirce
 print(f'The sale price is {convertToUSD(sale_price)}')
--- chapter9/string_input.py
+++ chapter9/string_input.py
@@ -6,10 +6,36 @@
 # helper function that returns an input function that checks
 # if the value entered is a valid type of either number or name,
 # and then storing the values in an instance of the Person class.
+# I will also be generating a greeting depending on the time, using
+# Python3's built-in datetime module.
+
+# Importing the datetime module
+import datetime 
+
+# Using the datetime module, I am getting the current date and time
+# according to the computer's clock and storing only the hour in
+# military time.
+current_hour = datetime.datetime.now().hour
+
+# I am going to calculate whether it is morning, afternoon, evening,
+# or night by using the current_hour and logic operators, and returning
+# the appropriate salutation.
+def getGreeting(hour):
+    if hour >= 1 and hour <= 5:
+        return "You're sure up early"
+    if hour >= 5 and hour <= 11:
+        return "Good morning"
+    if hour >= 12 and hour <= 17:
+        return "Good afternoon"
+    if hour >= 18 and hour <= 21:
+        return "Good evening"
+    if hour >= 22 and hour <= 24:
+        return "You're sure up early"
+
+greeting = getGreeting(current_hour)
 
 # Importing the generateInput() helper function from
-# helper_functions.py
-
+# helper_functions.py and Person class from Person_class.py
 from helper_functions import generateInput
 from Person_class import Person
 
@@ -30,5 +56,5 @@ user = Person(user_first_name, user_last_name)
 # to add an exclaimation mark (!) at the end of the
 # output and then break into a new line for following
 # prints.
-print('Hello', user.first_name, user.last_name, end="!\n")
+print(greeting, user.first_name, user.last_name, end="!\n")
 
