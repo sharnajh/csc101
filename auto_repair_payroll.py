fc3b0f4d71b5a7ff7731f411982c6f4f5bbb253b
Sharna Hossain <sharnajh@gmail.com>
2020-07-27 23:30:06 -0400

    finished

--- auto_repair_payroll.py
+++ auto_repair_payroll.py
@@ -3,48 +3,50 @@
 # Chapter 10 | Program 2
 # auto_repair_payroll.py
 
-# Named constants to represent the base hours and
-# the overtime multiplier
-BASE_HOURS = 40         # Base hours per week
-OT_MULTIPLIER = 1.5     # Overtime multiplier
+# import random module to generate random nums
+import random
 
-# Function that checks if input is valid data type
+# Named constant to represent the tax
+TAX = 0.35
+
+# Create class for employees that take in values for name
+# and their rate of pay according to their position
+class Employee:
+    # __init__ is the function that when executed
+    # will create a new instance of the class with
+    # the parameters as the data
+    def __init__(self, name, position, hours_worked):
+        # Format name so it is capitalized
+        self.name = name.title()
+        self.pay_rate = position
+        self.hours_worked = hours_worked
 
 
-def generateFloatInput(message):
-    value = input(f'{message}\t')
-    # If input has any letters in it, will continue
-    # to keep asking for input until a valid float
-    # is entered.
-    while value.isalpha():
-        print(f'"{value}" is not a valid float')
-        value = input(f'{message}')
-    return value
+# Create dictionary for employees
+employees = {}
 
+# Using a for loop to generate 5 employee objects with data
+# received from input funtions
+for x in (range(1, 6)):
+    # get employee name
+    employee_name = input(f"What is employee {x}'s name?\t")
+    # get employee pay rate
+    employee_pay_rate = float(input(f"What is employee {x}'s pay rate?\t"))
+    # get employee hours worked
+    employee_hours_worked = float(
+        input(f"How many hours did employee {x} work?\t"))
+    # create new instane of Employee class with the received data
+    employees[employee_name] = Employee(
+        employee_name, employee_pay_rate, employee_hours_worked)
 
-# Get the hours worked and the hourly pay rate.
-# Using generateFloatInput() to check valid data types.
-hours = float(generateFloatInput('Enter the number of hours worked:'))
-pay_rate = float(generateFloatInput('Enter the hourly pay rate:'))
+# Display the tax rate
+print(f'The tax rate is {TAX*100}%')
 
-# Calculate and display the gross pay.
-if hours > BASE_HOURS:
-    # Calculate the gross pay with overtime.
-    # First, get the number of overtime hours worked.
-    overtime_hours = hours - BASE_HOURS
-    # Print how many overtime hours worked.
-    print(f'You worked {overtime_hours} hours of OT.')
-
-    # Calculate the amount of overtime pay.
-    overtime_pay = overtime_hours * pay_rate * OT_MULTIPLIER
-
-    # Calculate the gross pay.
-    gross_pay = BASE_HOURS * pay_rate + overtime_pay
-else:
-    # Calculate the gorss pay without overtime.
-    gross_pay = hours * pay_rate
-
-# Display the gross pay.
-# Used the F-string to inject python into string.
-gross_pay = format(gross_pay, ",.2f")
-print(f'The gross pay is ${gross_pay}')
+# looping through employees dictionary to calculate the gross and netpay of each employee and display data
+for x in employees.values():
+    # calculate gross pay
+    x.gross_pay = x.hours_worked * x.pay_rate
+    # calculate net pay
+    x.net_pay = x.gross_pay - (x.gross_pay * TAX)
+    # printing all of the details of each employee
+    print(f'{x.name} has a pay rate of ${x.pay_rate}. After working {x.hours_worked} hours, they made a gross of ${format(x.gross_pay,",.2f")} and a net of ${format(x.net_pay,",.2f")}\n')
