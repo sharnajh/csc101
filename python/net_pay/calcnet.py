9714a96ed9e9d3cbcf1cacade311d5f38a6d05e8
Sharna Hossain <sharnajh@gmail.com>
2020-08-10 19:25:37 -0400

    animation change

--- python/net_pay/calcnet.py
+++ python/net_pay/calcnet.py
@@ -2,11 +2,19 @@
 # Sharna Hossain
 # module for net_pay.py
 
-# Value of tax
-TAX = 0.15
+
+def calculate_gross(hours, rate):
+    return float(hours * rate)
 
 
-def get_info():
+def calculate_net(gross, tax):
+    # Deduct taxes to calculate the net
+    gross -= float(gross * tax)
+    # Return gross
+    return gross
+
+
+def get_info(tax):
     # Create empty dictionary to store employee info
     employee = {}
     # Get input data
@@ -16,20 +24,9 @@ def get_info():
     # Calculate the gross from input data
     employee["gross"] = calculate_gross(employee["hours"], employee["rate"])
     # Calculate the net from input data
-    employee["net"] = calculate_net(employee["gross"])
+    employee["net"] = calculate_net(employee["gross"], tax)
     # Return employee dictionary
     return employee
-
-
-def calculate_gross(hours, rate):
-    return float(hours * rate)
-
-
-def calculate_net(gross):
-    # Deduct taxes to calculate the net
-    gross -= float(gross * TAX)
-    # Return gross
-    return gross
 
 
 def display_info(employee):
--- python/net_pay/main.py
+++ python/net_pay/main.py
@@ -7,12 +7,15 @@
 # Import module with functions
 import calcnet
 
+
 # Main function
 
 
 def main():
+    # Value of tax
+    TAX = 0.15
     # Get employee info and store into local var
-    employee = calcnet.get_info()
+    employee = calcnet.get_info(TAX)
     # Print the information
     calcnet.display_info(employee)
 
