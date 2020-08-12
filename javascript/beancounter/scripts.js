65dd812e958839d5c70c94770dca9698fdca09f0
Sharna Hossain <sharnajh@gmail.com>
2020-08-11 22:06:54 -0400

    finished bean counter

--- javascript/beancounter/scripts.js
+++ javascript/beancounter/scripts.js
@@ -1,26 +1,3 @@
-// let drink = "latte";
-// (ounce = 12), (shots = 2), (taxRate = 0.88), (price = 0);
-
-// if (drink.toLowerCase() == "espresso") {
-//   price = 1.4;
-// } else if (
-//   drink.toLowerCase() == "latte" ||
-//   drink.toLowerCase() == "cappuccino"
-// ) {
-//   if (ounce == 8) {
-//     price = 1.95;
-//   } else if (ounce == 12) {
-//     price = 2.35;
-//   } else if (ounce == 16) {
-//     price = 2.75;
-//   }
-// } else if (drink.toLowerCase() == "americano") {
-//   price = 1.2 + 0.3 * (ounce / 8);
-// }
-
-// price += (shots - 1) * 0.5;
-// price += price * taxRate;
-
 // Shots
 const shotInput = document.getElementById("shot-input");
 const shotBtns = document.querySelectorAll(".shots");
@@ -59,3 +36,48 @@ clearBtn.addEventListener("click", () => {
   clear();
 });
 
+// Calculate Function
+const totalBtn = document.getElementById("total-btn");
+const priceInput = document.getElementById("price-input");
+const calcPrice = () => {
+  // Set up all values necessary to calculate price
+  const taxRate = 0.88;
+  let drink = typeInput.value.toLowerCase();
+  let shots = shotInput.value;
+  let ounce =
+    // ES6 Shorthand for if-else statements
+    sizeInput.value == "S"
+      ? 8
+      : sizeInput.value == "T"
+      ? 12
+      : sizeInput.value == "G"
+      ? 16
+      : 0;
+  let price;
+
+  // Calculate function with nested if-else statements
+  if (drink == "espresso") {
+    price = 1.4;
+  } else if (drink == "latte" || drink == "cappuccino") {
+    if (ounce == 8) {
+      price = 1.95;
+    } else if (ounce == 12) {
+      price = 2.35;
+    } else if (ounce == 16) {
+      price = 2.75;
+    }
+  } else if (drink == "americano") {
+    price = 1.2 + 0.3 * (ounce / 8);
+  }
+  price += (shots - 1) * 0.5;
+  price += price * taxRate;
+  priceInput.value = price.toFixed(2);
+};
+
+totalBtn.addEventListener("click", () => {
+  // Check if all input fields have been entered
+  if (typeInput.value == "" || sizeInput.value == "" || shotInput.value == "") {
+    return;
+  }
+  calcPrice();
+});
