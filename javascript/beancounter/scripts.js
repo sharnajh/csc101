37bba452cde6acca5d7dfb168feb709159dac3f3
Sharna Hossain <sharnajh@gmail.com>
2020-08-11 21:05:57 -0400

    bean counter

--- javascript/beancounter/scripts.js
+++ javascript/beancounter/scripts.js
@@ -1,24 +1,61 @@
-let drink = "latte";
-(ounce = 12), (shots = 2), (taxRate = 0.88), (price = 0);
+// let drink = "latte";
+// (ounce = 12), (shots = 2), (taxRate = 0.88), (price = 0);
 
-if (drink.toLowerCase() == "espresso") {
-  price = 1.4;
-} else if (
-  drink.toLowerCase() == "latte" ||
-  drink.toLowerCase() == "cappuccino"
-) {
-  if (ounce == 8) {
-    price = 1.95;
-  } else if (ounce == 12) {
-    price = 2.35;
-  } else if (ounce == 16) {
-    price = 2.75;
-  }
-} else if (drink.toLowerCase() == "americano") {
-  price = 1.2 + 0.3 * (ounce / 8);
+// if (drink.toLowerCase() == "espresso") {
+//   price = 1.4;
+// } else if (
+//   drink.toLowerCase() == "latte" ||
+//   drink.toLowerCase() == "cappuccino"
+// ) {
+//   if (ounce == 8) {
+//     price = 1.95;
+//   } else if (ounce == 12) {
+//     price = 2.35;
+//   } else if (ounce == 16) {
+//     price = 2.75;
+//   }
+// } else if (drink.toLowerCase() == "americano") {
+//   price = 1.2 + 0.3 * (ounce / 8);
+// }
+
+// price += (shots - 1) * 0.5;
+// price += price * taxRate;
+
+// Shots
+const shotInput = document.getElementById("shot-input");
+const shotBtns = document.querySelectorAll(".shots");
+for (const button of shotBtns) {
+  button.addEventListener("click", () => {
+    shotInput.value = button.value;
+  });
 }
 
-price += (shots - 1) * 0.5;
-price += price * taxRate;
+// Sizes
+const sizeInput = document.getElementById("size-input");
+const sizeBtns = document.querySelectorAll(".size");
+for (const button of sizeBtns) {
+  button.addEventListener("click", () => {
+    sizeInput.value = button.value;
+  });
+}
 
-document.getElementById("price").innerHTML = `Price: $${price.toFixed(2)}`;
+// Types
+const typeInput = document.getElementById("type-input");
+const typeBtns = document.querySelectorAll(".type");
+for (const button of typeBtns) {
+  button.addEventListener("click", () => {
+    typeInput.value = button.value;
+  });
+}
+
+// Clear function
+const clearBtn = document.getElementById("clear");
+const clear = () => {
+  typeInput.value = "";
+  shotInput.value = "";
+  sizeInput.value = "";
+};
+clearBtn.addEventListener("click", () => {
+  clear();
+});
+
